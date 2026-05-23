83c1089c32d2a43752b8f894b998e69af84c0c03
bhanuprasadbk <bhanubommalakunta@gmail.com>
2026-05-23 15:25:53 +0530

    code comitted

--- src/config/env.ts
+++ src/config/env.ts
@@ -16,7 +16,7 @@ function requireEnv(name: keyof ImportMetaEnv): string {
 }
 
 const DEFAULT_API_BASE_URL =
-  'https://healthlynkapi-e9dzgrfgaebvhre4.centralus-01.azurewebsites.net/api'
+  'https://healthlynkapidev-h0ggfvfkeubmg6c3.centralus-01.azurewebsites.net/api'
 
 function readRuntimeApiBaseUrl(): string {
   if (typeof window === 'undefined') return ''
